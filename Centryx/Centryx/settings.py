2af10d553cdc05d0cd6e6c92ca6d2363ac30e5ac
antoineleno <lenoantoine2000@gmail.com>
2025-10-05 19:45:14 +0800

    Adding api endpoints

--- Centryx/Centryx/settings.py
+++ Centryx/Centryx/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "djoser",
     "rest_framework",
+    # needed for rotating/blacklisting refresh tokens (logout support)
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 MIDDLEWARE = [
@@ -135,11 +137,10 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     # short lived access token, longer lived refresh token
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
+    # rotate refresh tokens on use and blacklist old ones to support logout
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
-
-
-
-
