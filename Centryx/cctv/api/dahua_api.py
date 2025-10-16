7e1c8da83ea0141b04ca9983f30b45e747903951
antoineleno <lenoantoine2000@gmail.com>
2025-10-17 04:28:25 +0800

    Initialising api files

---
+++ Centryx/cctv/api/dahua_api.py
@@ -0,0 +1,30 @@
+class DahuaAPI:
+    """DahuaAPI class to interact with Dahua cameras
+    """
+    def __init__(self):
+        """Initialize the DahuaAPI class."""
+        pass
+
+    def get_access_token(self):
+        """Get access token for API authentication."""
+        pass
+
+    def add_camera(self):
+        """Add a new camera to the system."""
+        pass
+
+    def remove_camera(self):
+        """Remove a camera from the system."""
+        pass
+    
+    def get_motion(self):
+        """Get motion detection status."""
+        pass
+
+    def get_live_stream(self):
+        """Get live stream footage."""
+        pass
+
+    def get_recordings(self):
+        """Get recorded footage."""
+        pass
